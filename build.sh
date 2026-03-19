c6226459e10d09d42d36e18afd476e4924a154a6
Iyyappan-M <iyyappan16112001@gmail.com>
2026-03-19 22:20:30 +0530

    Final fix: Ensure start shim is executable via build script

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 
+chmod +x build.sh
+chmod +x "diabetes_detection_system.wsgi:application"
+
 python manage.py collectstatic --no-input
 python manage.py migrate
